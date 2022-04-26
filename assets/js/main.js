164800c46c3baf11f5d6415a2714522175f3cd3b
Sydur Rahman <sydurrahmant1@gmail.com>
2022-04-27 03:19:20 +0600

    hero Slider And Category Structure Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,35 @@
 (function ($) {
   "use strict";
 
-  //  Slider slider -2
- 
-  
+  //  Slider slider
+    $('.hero-slider').owlCarousel({
+      items: 1,
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      animateOut: 'fadeOut',
+      autoplayTimeout: 8000,
+      dots: true,
+      nav: false,
+      lazyLoad: true,
+      // autoplayTimeout: 3000,
+      //  autoplaySpeed: 3000, 
+  });
+
+  //  slide-feature-1
+    $('.slide-feature-1').owlCarousel({
+      items: 6,
+      margin: 30,
+      loop: false,
+      autoplay: true,
+      animateOut: 'fadeOut',
+      autoplayTimeout: 8000,
+      dots: false,
+      nav: true,
+      lazyLoad: true,
+      // autoplayTimeout: 3000,
+      //  autoplaySpeed: 3000, 
+  });
+
 
 })(jQuery);
